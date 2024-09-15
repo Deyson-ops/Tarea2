4fdfe35aa6216a2df2e44e49c5ea4266932b2643
Deyson-ops <ddonadol@miumg.edu.gt>
2024-09-15 00:38:06 -0600

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
+    <div className="container mt-4">
       <h1 className="text-center">Bienvenido a Cursos UMG</h1>
       <br/>
-      <p className="justified-text">Para realizar este proyecto utilizamos las funcionalidades de React y Bootstrap para implementar una página que brinda información de los cursos finalizados en nuestra carrera con detalles específicos sobre cada uno.</p>
+      <p className="justified-text">
+        Para realizar este proyecto utilizamos las funcionalidades de React y Bootstrap para implementar una página que brinda información de los cursos finalizados en nuestra carrera con detalles específicos sobre cada uno.
+      </p>
       <img 
-  src="https://i.pinimg.com/originals/34/22/90/3422900cb9e9bd4ce803847129eb6c9f.jpg" 
-  alt="Logo" 
-  style={{ 
-    width: '450px', 
-    height: 'auto', 
-    display: 'block', 
-    margin: '0 auto' 
-  }} 
-/>
+        src="https://i.pinimg.com/originals/34/22/90/3422900cb9e9bd4ce803847129eb6c9f.jpg" 
+        alt="Logo" 
+        style={{ 
+          width: '450px', 
+          height: 'auto', 
+          display: 'block', 
+          margin: '0 auto' 
+        }} 
+        className="img-fluid"
+      />
     </div>
   );
 }
